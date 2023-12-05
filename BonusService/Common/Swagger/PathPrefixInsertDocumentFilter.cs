34b8469f87a093477a014bb0191ebc66afccd0db
Orekhov Evgeniy <eorekhov@sitronics.com>
2023-12-05 18:05:47 +0300

    refactoring

---
+++ BonusService/Common/Swagger/PathPrefixInsertDocumentFilter.cs
@@ -0,0 +1,24 @@
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+namespace BonusService.Common.Swagger;
+
+public class PathPrefixInsertDocumentFilter : IDocumentFilter
+{
+    private readonly string _pathPrefix;
+
+    public PathPrefixInsertDocumentFilter(string prefix)
+    {
+        this._pathPrefix = prefix;
+    }
+
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        var paths = swaggerDoc.Paths.Keys.ToList();
+        foreach (var path in paths)
+        {
+            var pathToChange = swaggerDoc.Paths[path];
+            swaggerDoc.Paths.Remove(path);
+            swaggerDoc.Paths.Add($"{_pathPrefix}{path}", pathToChange);
+        }
+    }
+}
